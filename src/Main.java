112c11eba3e5b94386a158c497feb034454dd62b
sofarinal <sofa.markis@yandex.ru>
2025-12-27 22:00:46 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,87 @@
+import java.time.Year;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            //Задание 1
+            int age = 19;
+            if (age >= 18) {
+                System.out.println("Если возраст человека равен " + age + " то он достиг совершеннолетия");
+            }
+            if (age < 18) {
+                System.out.println("Если возраст человека равен " + age + " то он не достиг совершеннолетия, нужно немного подождать");
+            }
+
+            // Задание 2
+        int weather = 8;
+            if (weather >= 5) {
+                System.out.println("На улиуце " + weather + " можно идти без шапки");
+            }
+            if (weather < 5) {
+                System.out.println("На улице " + weather + " нужно надеть шапку");
+            }
+
+            // Задание 3
+        int speed = 90;
+            if (speed >= 60) {
+                System.out.println("Если скорость " + speed + " придется заплатить штраф");
+            }
+            if (speed < 60) {
+                System.out.println("Если скорость " + speed + " можно ездить спокойно");
+            }
+
+            // Задание 4
+        int year = 30;
+            if (year >= 2 && year <= 6) {
+                System.out.println("Если возраст человека равен " + year + " то ему нужно ходить в детский сад");
+            }
+            if (year >= 7 && year <= 17) {
+                System.out.println("Если возраст человека равен " + year + " то ему нужно ходить в школу");
+        }
+            if (year >= 18 && year <= 24) {
+                System.out.println("Если возраст человека равен " + year + " то ему нужно ходить в университет");
+            }
+            if (year >= 24)
+                System.out.println("Если возраст человека равен " + year + " то ему нужно ходить на работу");
+
+            // Задача 5
+        int children = 17;
+        if (children < 5) {
+            System.out.println("Если возраст человека равен " + children + " то ему нельзя кататься");
+        } else  if (children <= 14) {
+            System.out.println("Если возраст человека равен " + children + " то ему можно кататься в сопровождении");
+        } else {
+            System.out.println("Если возраст человека равен " + children + " то ему можно кататься без сопровождения");
+        }
+
+            // Задание 6
+        int allPlaces = 102;
+        int seatingPositions = 60;
+        int passenger = 184;
+        if (passenger >= allPlaces) {
+            System.out.println("Вагон полностью значит, мест нет ");
+        } else if (passenger < seatingPositions) {
+            int availableSeats = seatingPositions - passenger;
+            System.out.println("Есть " + availableSeats + " свободных сидячих мест");
+        } else {
+            int places = allPlaces - passenger;
+            System.out.println("Сидячих мест нет, но есть " + places + " стоячих мест");
+        }
+
+            // Задание 7
+        int max;
+        int one = 19;
+        int two = 28;
+        int three = 2;
+        if (one >= two && one >= three) {
+            max = one;
+        } else if (two >= one && two >= three) {
+            max = two;
+        } else {
+            max = three;
+        }
+        System.out.println("Наибольшее число: " + max);
         }
     }
-}
